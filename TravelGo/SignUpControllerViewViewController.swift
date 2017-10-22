d034356c5e167a8be69f5d6ef21d013b37bb2838
Nulrybek Karshyga <nulrybekkarshyga@MacBook-Pro-Nulrybek.local>
2017-10-21 22:57:12 -0400

    12345

---
+++ TravelGo/SignUpControllerViewViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SignUpControllerViewViewController.swift
+//  TravelGo
+//
+//  Created by Nulrybek Karshyga on 21.10.17.
+//  Copyright © 2017 Xavi Loinaz. All rights reserved.
+//
+
+import UIKit
+
+class SignUpControllerViewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- TravelGo/ViewController.swift
+++ TravelGo/ViewController.swift
@@ -7,9 +7,40 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+
 
 class ViewController: UIViewController {
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
 
+    @IBAction func LoginPressed(_ sender: UIButton) {
+        if emailTextField.text == "" || passwordTextField.text == ""{
+            let alertController = UIAlertController(title: "Error", message: "Please enter an email and password.", preferredStyle: .alert)
+            let alertAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(alertAction)
+            
+            self.present(alertController, animated: true, completion: nil)
+        } else{
+            Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!, completion: { (user, error) in
+                if error == nil{
+                    print("You have successfully loged in")
+                    let vc = self.storyboard?.instantiateViewController(withIdentifier: "Flight")
+                    self.present(vc!, animated: true, completion: nil)
+                } else{
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    let alertAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(alertAction)
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                    
+                }
+            })
+            
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
