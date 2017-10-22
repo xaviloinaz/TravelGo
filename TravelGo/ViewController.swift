c86b9f2d68ac300e6efdd405dbd038ce86799206
Xavi Loinaz <x.e.loinaz@gmail.com>
2017-10-21 22:34:44 -0400

    Initial Commit

---
+++ TravelGo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TravelGo
+//
+//  Created by Xavi Loinaz on 10/21/17.
+//  Copyright © 2017 Xavi Loinaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
